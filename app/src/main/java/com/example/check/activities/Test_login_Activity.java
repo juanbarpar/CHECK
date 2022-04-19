73e07409008e72cdb0825822bf786b49ce249bb1
NachoF1M <81889464+NachoF1M@users.noreply.github.com>
2022-04-19 11:19:57 -0500

    login
    login

---
+++ app/src/main/java/com/example/check/activities/Test_login_Activity.java
@@ -0,0 +1,16 @@
+package com.example.check.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.check.R;
+
+public class Test_login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_login);
+    }
+}
