e416ab0e7303b5ca2e7280a7f97735e70a4d8b0d
NachoF1M <81889464+NachoF1M@users.noreply.github.com>
2022-04-19 04:12:41 -0500

    Posible login

---
+++ app/src/main/java/com/example/check/Activities/TestSignUpActivity.java
@@ -0,0 +1,16 @@
+ package com.example.check.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.check.R;
+
+ public class TestSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_sign_up);
+    }
+}
