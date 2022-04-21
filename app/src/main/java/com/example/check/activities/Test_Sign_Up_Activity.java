15560c75fe88bfe53119a50016982d869f7676dc
NachoF1M <81889464+NachoF1M@users.noreply.github.com>
2022-04-21 18:05:28 -0500

    Registro

---
+++ app/src/main/java/com/example/check/activities/Test_Sign_Up_Activity.java
@@ -0,0 +1,16 @@
+package com.example.check.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.check.R;
+
+public class Test_Sign_Up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_sign_up);
+    }
+}
