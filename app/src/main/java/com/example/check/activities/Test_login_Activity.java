392aec5d93db9985bb5527466d279bc5b422992a
NachoF1M <81889464+NachoF1M@users.noreply.github.com>
2022-04-22 02:38:31 -0500

    funcionalidad_Registro

--- app/src/main/java/com/example/check/activities/Test_login_Activity.java
+++ app/src/main/java/com/example/check/activities/Test_login_Activity.java
@@ -2,15 +2,26 @@ package com.example.check.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.check.R;
+import com.example.check.databinding.ActivityTestLoginBinding;
 
 public class Test_login_Activity extends AppCompatActivity {
-
+    private  ActivityTestLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_login);
+        binding = ActivityTestLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+
     }
+    private void  setListeners() {
+        binding.buttonSignUp.setOnClickListener(view ->
+                startActivity(new Intent(getApplicationContext(),Test_Sign_Up_Activity.class)));
+   }
+
 }
