670a148e5d67fe2d39b917383ccf8f575a81859a
NachoF1M <81889464+NachoF1M@users.noreply.github.com>
2022-04-26 10:29:14 -0500

    Confirmacion_emailV1

--- app/src/main/java/com/example/check/activities/Test_Sign_Up_Activity.java
+++ app/src/main/java/com/example/check/activities/Test_Sign_Up_Activity.java
@@ -9,16 +9,24 @@ import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.check.Principal.LoginActivity;
 import com.example.check.Principal.MainActivity;
+import com.example.check.R;
 import com.example.check.Utilities.Constantes;
 import com.example.check.Utilities.PreferenceManager;
 import com.example.check.databinding.ActivityTestSignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -32,8 +40,11 @@ public class Test_Sign_Up_Activity extends AppCompatActivity {
     private String encodedImage;
     private PreferenceManager preferenceManager;
     private ActivityTestSignUpBinding binding;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         binding = ActivityTestSignUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -58,31 +69,34 @@ public class Test_Sign_Up_Activity extends AppCompatActivity {
     }
     private  void signUp(){
 
+        if(toSign(binding.inputEmail.getText().toString().trim(),binding.inputPassword.getText().toString().trim())){
 
+            loading(true);
+            FirebaseFirestore database = FirebaseFirestore.getInstance();
+            HashMap<String,Object> user = new HashMap<>();
+            user.put(Constantes.KEY_NAME, binding.inputName.getText().toString());
+            user.put(Constantes.KEY_EMAIL, binding.inputEmail.getText().toString().trim());
+            user.put(Constantes.KEY_PASSWORD, binding.inputPassword.getText().toString().trim());
+            user.put(Constantes.KEY_IMAGE, encodedImage);
 
-        loading(true);
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String,Object> user = new HashMap<>();
-        user.put(Constantes.KEY_NAME, binding.inputName.getText().toString());
-        user.put(Constantes.KEY_EMAIL, binding.inputEmail.getText().toString());
-        user.put(Constantes.KEY_PASSWORD, binding.inputPassword.getText().toString());
-        user.put(Constantes.KEY_IMAGE, encodedImage);
-        database.collection(Constantes.KEY_COLLECTION_USERS)
-                .add(user)
-                .addOnSuccessListener(documentReference -> {
-                    loading(false);
-                    preferenceManager.putBoolean(Constantes.KEY_IS_SIGNED_IN,true);
-                    preferenceManager.putString(Constantes.KEY_USER_ID,documentReference.getId());
-                    preferenceManager.putString(Constantes.KEY_NAME, binding.inputName.getText().toString());
-                    preferenceManager.putString(Constantes.KEY_IMAGE,encodedImage);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                })
-                .addOnFailureListener(exception -> {
-                    loading(false);
-                    showToast(exception.getMessage());
-                });
+            database.collection(Constantes.KEY_COLLECTION_USERS)
+                    .add(user)
+                    .addOnSuccessListener(documentReference -> {
+                        loading(false);
+                        preferenceManager.putBoolean(Constantes.KEY_IS_SIGNED_IN,true);
+                        preferenceManager.putString(Constantes.KEY_USER_ID,documentReference.getId());
+                        preferenceManager.putString(Constantes.KEY_NAME, binding.inputName.getText().toString());
+                        preferenceManager.putString(Constantes.KEY_IMAGE,encodedImage);
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    })
+                    .addOnFailureListener(exception -> {
+                        loading(false);
+                        showToast(exception.getMessage());
+                    });
+        }
+
     }
 
     private String encodeImage(Bitmap bitmap){
@@ -123,7 +137,7 @@ public class Test_Sign_Up_Activity extends AppCompatActivity {
         }else if(binding.inputEmail.getText().toString().trim().isEmpty()){
             showToast("Ingresa un email");
             return false;
-        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString().trim()).matches()){
             showToast("Ingresa un email valido");
             return  false;
         }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
@@ -134,9 +148,6 @@ public class Test_Sign_Up_Activity extends AppCompatActivity {
             return false;
         }else if(!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())){
             showToast("La contraseña y la confirmacion de contraseña deben ser iguales");
-            return false;
-        }else if(Constantes.KEY_EMAIL_CONFIRM!=""){
-            showToast("Este correo ya se encuentra registrado");
             return false;
         }else{
             return true;
@@ -153,4 +164,41 @@ public class Test_Sign_Up_Activity extends AppCompatActivity {
             binding.buttonSignUp.setVisibility(View.VISIBLE);
         }
     }
+    boolean is;
+    public boolean toSign(String u, String p){
+
+
+        String user = u;
+
+
+        String pass = p;
+
+        if(!user.equals("") && !pass.equals("")){
+            mAuth.createUserWithEmailAndPassword(user, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                is = task.isSuccessful();
+                            } else {
+
+                            }
+                        }
+                    })
+            .addOnFailureListener(exception -> {
+                showToast("El email ya se encuentra registrado");
+            });
+        }
+        else{
+
+            return  false;
+
+
+
+        }
+    return is;
+    }
+
 }
